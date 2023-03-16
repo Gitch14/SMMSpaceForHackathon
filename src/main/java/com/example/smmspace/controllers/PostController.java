a390de121e5ceb246ca3f359740b1cd6efb1a94f
Gitch14 <vladbuckach@ukr.net>
2023-03-17 00:16:06 +0200

    +fix user info

--- src/main/java/com/example/smmspace/controllers/PostController.java
+++ src/main/java/com/example/smmspace/controllers/PostController.java
@@ -1,9 +1,8 @@
 package com.example.smmspace.controllers;
 
-import com.example.smmspace.models.Image;
 import com.example.smmspace.models.Post;
 import com.example.smmspace.models.User;
-import com.example.smmspace.repositories.ImageRepository;
+import com.example.smmspace.repositories.PostRepository;
 import com.example.smmspace.services.Impl.PostServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -17,48 +16,39 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class PostController {
     private final PostServiceImpl postServiceImpl;
-    private final ImageRepository imageRepository;
+    private final PostRepository postRepository;
 
     @GetMapping("/")
-    public String products(@RequestParam(name = "searchWord", required = false) String title, Principal principal, Model model) {
-        model.addAttribute("posts", postServiceImpl.listProducts(title));
+    public String products(@RequestParam(name = "searchWord", required = false) String text, Principal principal, Model model) {
+
+        if (text != null) {
+            List<Post> titleSearch = postRepository.findByTitle(text);
+            List<Post> typeSearch = postRepository.findByType(text);
+            if (titleSearch.size() == 0 && typeSearch.size() == 0){
+                model.addAttribute("posts", Collections.emptyList());
+            } else {
+
+                List<Post> filteredCourses = new ArrayList<>();
+                filteredCourses.addAll(titleSearch);
+                filteredCourses.addAll(typeSearch);
+                model.addAttribute("posts", filteredCourses);
+            }
+        } else {
+            model.addAttribute("posts", postRepository.findAll());
+        }
+        model.addAttribute("searchWord", text);
         model.addAttribute("user", postServiceImpl.getUserByPrincipal(principal));
-        model.addAttribute("searchWord", title);
+
         return "posts";
     }
-/*
-    @GetMapping("/post/{id}")
-    public String productInfo(@PathVariable Long id, Model model, Principal principal, HttpServletRequest request) {
-        Post post = postServiceImpl.getProductById(id);
-        model.addAttribute("user", postServiceImpl.getUserByPrincipal(principal));
-        model.addAttribute("post", post);
-        model.addAttribute("images", post.getImages());
-        model.addAttribute("authorPost", post.getUser());
-        model.addAttribute("images", post.getImages());
-        // model.addAttribute("contextPath", request.getContextPath());
-        return "posts-info";
-    }
-
-
-
-    @GetMapping("/images")
-    public String getImageList(Model model) {
-        // Получение списка изображений из базы данных
-        List<Image> images = imageRepository.findAll();
-
-        // Добавление списка изображений в объект модели
-        model.addAttribute("images", images);
-
-        // Возврат имени представления для отображения списка изображений
-        return "posts-info";
-    }
-*/
 
     @GetMapping("/post/{id}")
     public String productInfo(@PathVariable Long id, Model model, Principal principal, HttpServletRequest request) {
@@ -71,8 +61,6 @@ public class PostController {
         model.addAttribute("requestContext", contextPath);
         return "posts-info";
     }
-
-
 
 
     @PostMapping("/post/create")
--- src/main/java/com/example/smmspace/repositories/PostRepository.java
+++ src/main/java/com/example/smmspace/repositories/PostRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByTitle(String title);
+    List<Post> findByType(String string);
 }
